7ea332b30801722ebb10b41be10fb8f0a51f16b5
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-04-08 10:14:01 -0700

    Page is now refreshing occasionally...

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,7 +3,6 @@
 var app = {
   server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
   roomsCollector: []
-
 };
 
 app.init = function() {
@@ -12,7 +11,6 @@ app.init = function() {
 };
 
 app.send = function(data) {
-
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: app.server,
@@ -21,7 +19,6 @@ app.send = function(data) {
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent', data);
-      app.fetch();
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -31,24 +28,28 @@ app.send = function(data) {
 };
 
 app.fetch = function(data) {
-
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: app.server,
     type: 'GET',
     // data: JSON.stringify(data),
     data: {
-      limit: 1000,
+      limit: 10,
       order: '-updatedAt'
     },
     dataType: 'json',
     contentType: 'application/json',
     success: function (data) {
-      console.log(data.results);
       var user = data.results;
-      for (var i = 0; i < user.length; i++) {
-        app.renderMessage(user[i]);
+      var index = user.length - 1;
+      console.log(user);
+
+      while (index >= 0) {
+        app.renderMessage(user[index]);
+        index -= 1;
       }
+      
+      
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -67,17 +68,17 @@ app.renderMessage = function(message) {
   var userText = message.text;
   var userRoomName = message.roomname;
   var userObjID = message.objectId;
-  $('#chats').append('<p class="username">' + userName + ': ' + userText + '</p><br>');
-  
+  var userCreated = message.updatedAt;
+  $('#chats').prepend('<p class="username">' + userName + ': ' + userText + '</p><br>');
   app.renderRoom(userRoomName);  
 };
 
 
 app.renderRoom = function(location) {
-  // $('#roomSelect').append('<p>' + location + '</p>');
   if (!_.contains(app.roomsCollector, location)) {
     app.roomsCollector.push(location);
     $('#roomSelect').find('#chat-rooms').append('<option value="' + location + '">' + location + '</options>');
+
   }
 };
 
@@ -93,8 +94,10 @@ app.handleSubmit = function() {
 
 $(document).ready(function() {
   app.fetch();
+
   var myName = this.location.search.slice(10);
 
+  // Send a chat message
   $('#send').on('click', function() {
     var myText = $('#message').val();
     var myMessage = {
@@ -102,12 +105,21 @@ $(document).ready(function() {
       text: myText,
       roomname: 'lobby'
     };
-    
     app.send(myMessage);
+
+    app.fetch();
   });
 
-  // Refresh button here
+  // Select a lobby
+  
 
+  // Refresh button here
+  setInterval(function() {
+    app.clearMessages();
+    app.fetch();
+    window.scrollTo(0, 0);
+  }, 10000);
 
 
 });
+
