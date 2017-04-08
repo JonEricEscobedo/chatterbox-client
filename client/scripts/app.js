c75631ebc3a50af4142c22289fddd5e76ec06c02
Hack Reactor Student <student@hackreactor.com>
2017-04-07 17:22:24 -0700

    Pass all tests with the exception of 'events'
    Work in progress.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,71 @@
 // YOUR CODE HERE:
+// http://parse.sfm8.hackreactor.com/
+var app = {
+  server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages'
+};
+
+app.init = function() {
+
+};
+
+app.send = function(data) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(data),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function(data) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'GET',
+    data: JSON.stringify(data),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').empty();
+};
+
+app.renderMessage = function(message) {
+  var userName = message.username;
+  var userText = message.text;
+  var userRoomName = message.roomname;
+
+  // $('#chats').append("<p>" + userText + "</p>");
+  $('#chats').append('<p class="username">' + userName + ': ' + userText + '</p>');
+};
+
+app.renderRoom = function(location) {
+  $('#roomSelect').append('<p>' + location + '</p>');
+};
+
+
+app.handleUserNameClick = function() {
+  console.log('yo');
+  return true;
+};
+
+$(document).ready(function() {
+  $('.username').on('click', app.handleUserNameClick());
+});
+
